41803e36c83ab008c73f86d3d98c26d1715fbc3b
Mustafa Azad <mustafaazad533@gmail.com>
2024-05-30 10:14:42 +0530

    Move configuration variables to app/core/config.py

---
+++ app/core/config.py
@@ -0,0 +1,7 @@
+import os
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+PINECONE_INDEX = "aiplanet"
